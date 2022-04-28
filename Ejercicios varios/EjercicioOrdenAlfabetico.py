938035e009dd25526db4f0ed279e0daee5d8aaac
Bel camargo <beellcamargo@gmail.com>
2022-04-28 19:57:17 -0300

    ejercicio

--- Ejercicios varios/EjercicioOrdenAlfabetico.py
+++ Ejercicios varios/EjercicioOrdenAlfabetico.py
@@ -1,10 +1,10 @@
 palabras=[]
 palabra = str(input("Ingrese palabras, para salir ingrese 'exit': "))
 while palabra != "exit":
-    try:
+    if palabra.isalpha == True:
         palabras.append(palabra)
         palabra = str(input("Ingrese palabras, para salir ingrese 'exit': "))
-    except:
+    else:        
         print("Ingrese solo palabras por favor!")
         palabras.append(palabra)
         palabra = str(input("Ingrese palabras, para salir ingrese 'exit': "))
